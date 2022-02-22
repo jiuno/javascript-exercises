9fb6327cd850774d258db54a7dd52148e4c7eb6e
Jiuno <bruno.alliani@gmail.com>
2022-02-22 20:52:32 -0300

    Complete test 04.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args ) {
+    args.forEach((element) => {
+        for( let i = 0; i < arr.length; i++){                       
+            if ( arr[i] === element) { 
+                arr.splice(i, 1); 
+            }
+        } 
 
+    })
+    return arr
+    
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
