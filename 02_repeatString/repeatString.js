1ae80320ab7283c7103830cd059b0ae5912b80c8
Jiuno <bruno.alliani@gmail.com>
2022-02-21 21:49:24 -0300

    Complete repeatstring tests.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(str,num) {
+    if (num < 0 ) {
+        return "ERROR"        
+    } else {
+        
+        const list = [];
+        for (let i = 0; i < num; i++) {
+          list.push(str)   
+        }
+        return list.join("")
+    }
+    
 };
 
 // Do not edit below this line
