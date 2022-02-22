96371217bc90db4982430916163c98f0922ce462
Jiuno <bruno.alliani@gmail.com>
2022-02-22 19:54:15 -0300

    Complete test 03.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let letters = []
+    for (let i = 0; i < str.length; i++) {
+         letters.push(str[i])
+    };
+    lettersReverse = letters.reverse()
+  
+    return lettersReverse.join("")   
 };
 
 // Do not edit below this line
