d2249fa67e274d2ee96d03d7651b1e3b2c97921b
Jiuno <bruno.alliani@gmail.com>
2022-02-23 19:55:12 -0300

    Complete test 05.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+let sum = 0;
+if (typeof a !== "number" || typeof b !== "number") {
+    return "ERROR";    
+}
 
+if (a < 0 || b < 0 ) {
+    return "ERROR";    
+};
+if (a <= b ) {
+    for (let i = a; i <= b; i++){
+        sum = sum + i;
+            
+        };
+} else {
+    for (let i = b; i <= a; i++){
+        sum = sum + i;
+            
+        };
+}
+    return sum;
 };
 
 // Do not edit below this line
