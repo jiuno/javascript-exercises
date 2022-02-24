eb7d1d359a4da47ced1db2dc574ee2a07837a876
Jiuno <bruno.alliani@gmail.com>
2022-02-23 21:19:19 -0300

    Complete test 07.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  resInCelsius = (f - 32)*(5/9);
+  return Number(resInCelsius.toFixed(1));
 };
 
-const ctof = function() {
+const ctof = function(c) {
+  resInFarhrenheit = c * (9/5) + 32;
+  return Number(resInFarhrenheit.toFixed(1));
 
 };
 
